080b8095f4edfb9305958cc2071b117c1be64802
leclair-7 <andromeda.t@tutanota.com>
2018-10-15 11:38:57 -0500

    Initial commit, current working pygame examples

---
+++ magic_variables.py
@@ -0,0 +1,22 @@
+
+'''
+started out as 700 x 500
+
+'''
+# x dim
+WIDTH = 575
+# y_dim
+HEIGHT = 380
+
+FPS = 60
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+BROWN	 = ( 165,  42,  42)
+
+PI = 3.141592
+CIRCLE_RADIUS = 2
